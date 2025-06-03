f483f2b414793e910ac88112b4eeefcc55e4961c
Maeve Côté <maeve04.cote@gmail.com>
2025-06-02 22:46:36 -0400

    Completed the logic of Calculator.Evaluate and added it's tests.

--- CAS.Core/Calculator.cs
+++ CAS.Core/Calculator.cs
@@ -23,35 +23,40 @@ namespace CAS.Core
     /// <param name="symbolTable">The symbol table to associate variables with values.</param>
     /// <exception cref="KeyNotFoundException">A variable in the equation is not found in the symbol table.</exception>
     /// <returns>The result of the equation</returns>
-    public static double Evaluate(ASTNode root, Dictionary<string, double> symbolTable)
+    public static double Evaluate(ASTNode root, Dictionary<string, double>? symbolTable = null, 
+      Dictionary<string, Func<List<double>, double>>? customFunctionTable = null)
     {
+      // Default variables trick
+      symbolTable ??= new Dictionary<string, double>();
+      customFunctionTable ??= new Dictionary<string, Func<List<double>, double>>();
+
       // A Dictionnary that converts string functions to evaluate it.
-      var functionTable = new Dictionary<string, Func<List<ASTNode>, Dictionary<string, double>, double>>
+      var functionTable = new Dictionary<string, Func<List<ASTNode>, Dictionary<string, double>, Dictionary<string, Func<List<double>, double>>, double>>
       {
-          { "abs", (args, symTable) => Math.Abs(Evaluate(args[0], symTable)) },
-          { "sign", (args, symTable) => Math.Sign(Evaluate(args[0], symTable)) },
-          { "sqrt", (args, symTable) => Math.Sqrt(Evaluate(args[0], symTable)) },
-          { "ln", (args, symTable) => Math.Log(Evaluate(args[0], symTable)) },
-          { "log", (args, symTable) =>
+          { "abs", (args, symTable, customFunc) => Math.Abs(Evaluate(args[0], symTable, customFunc)) },
+          { "sign", (args, symTable, customFunc) => Math.Sign(Evaluate(args[0], symTable, customFunc)) },
+          { "sqrt", (args, symTable, customFunc) => Math.Sqrt(Evaluate(args[0], symTable, customFunc)) },
+          { "ln", (args, symTable, customFunc) => Math.Log(Evaluate(args[0], symTable, customFunc)) },
+          { "log", (args, symTable, customFunc) =>
               {
                   if (args.Count == 2)
-                      return Math.Log(Evaluate(args[1], symTable), Evaluate(args[0], symTable)); // log(base, value)
+                      return Math.Log(Evaluate(args[1], symTable, customFunc), Evaluate(args[0], symTable, customFunc)); // log(base, value)
                   else
-                      return Math.Log10(Evaluate(args[0], symTable)); // default to base 10
+                      return Math.Log10(Evaluate(args[0], symTable, customFunc)); // default to base 10
               }
           },
-          { "exp", (args, symTable) => Math.Exp(Evaluate(args[0], symTable)) },
-          { "sin", (args, symTable) => Math.Sin(Evaluate(args[0], symTable)) },
-          { "cos", (args, symTable) => Math.Cos(Evaluate(args[0], symTable)) },
-          { "tan", (args, symTable) => Math.Tan(Evaluate(args[0], symTable)) },
-          { "asin", (args, symTable) => Math.Asin(Evaluate(args[0], symTable)) },
-          { "acos", (args, symTable) => Math.Acos(Evaluate(args[0], symTable)) },
-          { "atan", (args, symTable) => Math.Atan(Evaluate(args[0], symTable)) },
-          { "floor", (args, symTable) => Math.Floor(Evaluate(args[0], symTable)) },
-          { "ceil", (args, symTable) => Math.Ceiling(Evaluate(args[0], symTable)) },
-          { "round", (args, symTable) => Math.Round(Evaluate(args[0], symTable)) },
-          { "min", (args, symTable) => args.Select(arg => Evaluate(arg, symTable)).Min() },
-          { "max", (args, symTable) => args.Select(arg => Evaluate(arg, symTable)).Max() }
+          { "exp", (args, symTable, customFunc) => Math.Exp(Evaluate(args[0], symTable, customFunc)) },
+          { "sin", (args, symTable, customFunc) => Math.Sin(Evaluate(args[0], symTable, customFunc)) },
+          { "cos", (args, symTable, customFunc) => Math.Cos(Evaluate(args[0], symTable, customFunc)) },
+          { "tan", (args, symTable, customFunc) => Math.Tan(Evaluate(args[0], symTable, customFunc)) },
+          { "asin", (args, symTable, customFunc) => Math.Asin(Evaluate(args[0], symTable, customFunc)) },
+          { "acos", (args, symTable, customFunc) => Math.Acos(Evaluate(args[0], symTable, customFunc)) },
+          { "atan", (args, symTable, customFunc) => Math.Atan(Evaluate(args[0], symTable, customFunc)) },
+          { "floor", (args, symTable, customFunc) => Math.Floor(Evaluate(args[0], symTable, customFunc)) },
+          { "ceil", (args, symTable, customFunc) => Math.Ceiling(Evaluate(args[0], symTable, customFunc)) },
+          { "round", (args, symTable, customFunc) => Math.Round(Evaluate(args[0], symTable, customFunc)) },
+          { "min", (args, symTable, customFunc) => args.Select(arg => Evaluate(arg, symTable, customFunc)).Min() },
+          { "max", (args, symTable, customFunc) => args.Select(arg => Evaluate(arg, symTable, customFunc)).Max() }
       };
 
       if (root.Token.Type is Number)
@@ -63,13 +68,24 @@ namespace CAS.Core
       if (root.Token.Type is Function)
       {
         string funcName = root.Token.Type.stringValue;
-        return functionTable[funcName](root.Children, symbolTable);
+        try
+        {
+          // Use the function table to find the function.
+          double funcResult = functionTable[funcName](root.Children, symbolTable, customFunctionTable);
+          return funcResult;
+        }
+        catch (KeyNotFoundException e)
+        {
+          // Use the custom function table if not found.
+          double funcResult = customFunctionTable[funcName](EvaluateArgs(root.Children, symbolTable, customFunctionTable));
+          return funcResult;
+        }
       }
 
       if (root.Token.Type is Operator)
       {
-        double left = Evaluate(root.Children[0], symbolTable);
-        double right = Evaluate(root.Children[1], symbolTable);
+        double left = Evaluate(root.Children[0], symbolTable, customFunctionTable);
+        double right = Evaluate(root.Children[1], symbolTable, customFunctionTable);
 
         return root.Token.Type.stringValue switch
         {
@@ -83,6 +99,16 @@ namespace CAS.Core
       }
 
       throw new InvalidOperationException("Unsupported token type, the node is not evaluable.");
+    }
+
+    private static List<double> EvaluateArgs(List<ASTNode> args,  Dictionary<string, double> symbolTable, Dictionary<string, Func<List<double>, double>> customFunctionTable)
+    {
+
+      List<double> evalArgs = new List<double>();
+      foreach (ASTNode node in args)
+        evalArgs.Add(Evaluate(node, symbolTable, customFunctionTable));
+
+      return evalArgs;
     }
   }
 }
---
+++ CAS.UT/CalculatorTest.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using CAS.Core;
+using CAS.Core.EquationParsing;
+
+namespace CAS.UT
+{
+  public class CalculatorTest
+  {
+    [Fact]
+    public void BasicArithmetic()
+    {
+      var equation1 = new ASTNode(Token.Operator("+"), new List<ASTNode>
+      {
+        new ASTNode(Token.Operator("-"), new List<ASTNode>
+        {
+          new ASTNode(Token.Number("5"), new List<ASTNode>()),
+          new ASTNode(Token.Number("2"), new List<ASTNode>())
+        }),
+        new ASTNode(Token.Number("3"), new List<ASTNode>())
+      });
+
+      var equation2 = new ASTNode(Token.Operator("/"), new List<ASTNode>
+      {
+          new ASTNode(Token.Operator("*"), new List<ASTNode>
+          {
+              new ASTNode(Token.Number("6"), new List<ASTNode>()),
+              new ASTNode(Token.Number("3"), new List<ASTNode>())
+          }),
+          new ASTNode(Token.Number("2"), new List<ASTNode>())
+      });
+
+      var equation3 = new ASTNode(Token.Operator("*"), new List<ASTNode>
+      {
+          new ASTNode(Token.Operator("+"), new List<ASTNode>
+          {
+              new ASTNode(Token.Number("1"), new List<ASTNode>()),
+              new ASTNode(Token.Number("2"), new List<ASTNode>())
+          }),
+          new ASTNode(Token.Number("3"), new List<ASTNode>())
+      });
+
+      var result1 = Calculator.Evaluate(equation1);
+      var result2 = Calculator.Evaluate(equation2);
+      var result3 = Calculator.Evaluate(equation3);
+
+      Assert.Equal(6, result1, 0.001);
+      Assert.Equal(9, result2, 0.001);
+      Assert.Equal(9, result3, 0.001);
+    }
+
+    [Fact]
+    public void Variables()
+    {
+      var equation1 = new ASTNode(Token.Operator("+"), new List<ASTNode>
+      {
+          new ASTNode(Token.Operator("+"), new List<ASTNode>
+          {
+              new ASTNode(Token.Operator("^"), new List<ASTNode>
+              {
+                  new ASTNode(Token.Variable("x"), new List<ASTNode>()),
+                  new ASTNode(Token.Number("2"), new List<ASTNode>())
+              }),
+              new ASTNode(Token.Operator("*"), new List<ASTNode>
+              {
+                  new ASTNode(Token.Number("2"), new List<ASTNode>()),
+                  new ASTNode(Token.Variable("x"), new List<ASTNode>())
+              })
+          }),
+          new ASTNode(Token.Number("1"), new List<ASTNode>())
+      });
+      Dictionary<string, double> symbolTable1 = new Dictionary<string, double> { { "x", 2 } };
+
+      var equation2 = new ASTNode(Token.Operator("+"), new List<ASTNode>
+      {
+          new ASTNode(Token.Operator("+"), new List<ASTNode>
+          {
+              new ASTNode(Token.Operator("^"), new List<ASTNode>
+              {
+                  new ASTNode(Token.Variable("a"), new List<ASTNode>()),
+                  new ASTNode(Token.Number("3"), new List<ASTNode>())
+              }),
+              new ASTNode(Token.Operator("^"), new List<ASTNode>
+              {
+                  new ASTNode(Token.Variable("b"), new List<ASTNode>()),
+                  new ASTNode(Token.Number("2"), new List<ASTNode>())
+              })
+          }),
+          new ASTNode(Token.Variable("c"), new List<ASTNode>())
+      });
+      Dictionary<string, double> symbolTable2 = new Dictionary<string, double> { { "a", 2 }, { "b", 3 }, { "c", 4 } };
+
+      var equation3 = new ASTNode(Token.Operator("+"), new List<ASTNode>
+      {
+          new ASTNode(Token.Operator("-"), new List<ASTNode>
+          {
+              new ASTNode(Token.Operator("*"), new List<ASTNode>
+              {
+                  new ASTNode(Token.Number("3"), new List<ASTNode>()),
+                  new ASTNode(Token.Operator("^"), new List<ASTNode>
+                  {
+                      new ASTNode(Token.Variable("x"), new List<ASTNode>()),
+                      new ASTNode(Token.Number("2"), new List<ASTNode>())
+                  })
+              }),
+              new ASTNode(Token.Operator("*"), new List<ASTNode>
+              {
+                  new ASTNode(Token.Number("4"), new List<ASTNode>()),
+                  new ASTNode(Token.Variable("x"), new List<ASTNode>())
+              })
+          }),
+          new ASTNode(Token.Number("5"), new List<ASTNode>())
+      });
+      Dictionary<string, double> symbolTable3 = new Dictionary<string, double> { { "x", 2 } };
+
+      var result1 = Calculator.Evaluate(equation1, symbolTable1);
+      var result2 = Calculator.Evaluate(equation2, symbolTable2);
+      var result3 = Calculator.Evaluate(equation3, symbolTable3);
+
+      Assert.Equal(9, result1, 0.001);
+      Assert.Equal(21, result2, 0.001);
+      Assert.Equal(9, result3, 0.001);
+    }
+
+    [Fact]
+    public void Functions()
+    {
+      var equation1 = new ASTNode(Token.Function("sin"), new List<ASTNode>
+      {
+          new ASTNode(Token.Variable("x"), new List<ASTNode>())
+      });
+      Dictionary<string, double> symbolTable1 = new Dictionary<string, double> { { "x", 2 } };
+
+      var equation2 = new ASTNode(Token.Function("log"), new List<ASTNode>
+      {
+          new ASTNode(Token.Number("100"), new List<ASTNode>()),
+          new ASTNode(Token.Number("10"), new List<ASTNode>())
+      });
+      Dictionary<string, double> symbolTable2 = new Dictionary<string, double> { { "x", 2 } };
+
+      var equation3 = new ASTNode(Token.Function("sqrt"), new List<ASTNode>
+      {
+          new ASTNode(Token.Operator("+"), new List<ASTNode>
+          {
+              new ASTNode(Token.Number("4"), new List<ASTNode>()),
+              new ASTNode(Token.Operator("*"), new List<ASTNode>
+              {
+                  new ASTNode(Token.Number("2"), new List<ASTNode>()),
+                  new ASTNode(Token.Variable("x"), new List<ASTNode>())
+              })
+          })
+      });
+      Dictionary<string, double> symbolTable3 = new Dictionary<string, double> { { "x", 2 } };
+
+      var result1 = Calculator.Evaluate(equation1, symbolTable1);
+      var result2 = Calculator.Evaluate(equation2, symbolTable2);
+      var result3 = Calculator.Evaluate(equation3, symbolTable3);
+
+      Assert.Equal(0.909297, result1, 0.001);
+      Assert.Equal(0.5, result2, 0.001);
+      Assert.Equal(2.828427, result3, 0.001);
+    }
+
+    [Fact]
+    public void CustomFunctions()
+    {
+      var customFunctionTable = new Dictionary<string, Func<List<double>, double>> 
+      {
+        { "f", (args) => (Math.Pow(args[0], 2) + 2 * args[0] + 3)},
+        { "g", (args) => (Math.Pow(Math.Sin(args[0]), 2) + Math.Pow(Math.Cos(args[0]), 2))},
+        { "h", (args) => (Math.Sqrt(4 + 2 * args[0]))}
+      };
+
+      var equation1 = new ASTNode(Token.Function("h"), new List<ASTNode>
+      {
+          new ASTNode(Token.Function("f"), new List<ASTNode>
+          {
+              new ASTNode(Token.Function("g"), new List<ASTNode>
+              {
+                  new ASTNode(Token.Variable("x"), new List<ASTNode>())
+              })
+          })
+      });
+      Dictionary<string, double> symbolTable1 = new Dictionary<string, double> { { "x", 2 } };
+
+      var equation2 = new ASTNode(Token.Function("f"), new List<ASTNode>
+      {
+          new ASTNode(Token.Function("g"), new List<ASTNode>
+          {
+              new ASTNode(Token.Variable("x"), new List<ASTNode>())
+          })
+      });
+      Dictionary<string, double> symbolTable2 = new Dictionary<string, double> { { "x", 2 } };
+
+      var equation3 = new ASTNode(Token.Function("f"), new List<ASTNode>
+      {
+          new ASTNode(Token.Operator("+"), new List<ASTNode>
+          {
+              new ASTNode(Token.Function("g"), new List<ASTNode>
+              {
+                  new ASTNode(Token.Variable("x"), new List<ASTNode>())
+              }),
+              new ASTNode(Token.Function("h"), new List<ASTNode>
+              {
+                  new ASTNode(Token.Variable("y"), new List<ASTNode>())
+              })
+          })
+      });
+      Dictionary<string, double> symbolTable3 = new Dictionary<string, double> { { "x", 2 }, { "y", 3 } };
+
+      var result1 = Calculator.Evaluate(equation1, symbolTable1, customFunctionTable);
+      var result2 = Calculator.Evaluate(equation2, symbolTable2, customFunctionTable);
+      var result3 = Calculator.Evaluate(equation3, symbolTable3, customFunctionTable);
+
+      Assert.Equal(4, result1, 0.001);
+      Assert.Equal(6, result2, 0.001);
+      Assert.Equal(28.64911, result3, 0.001);
+    }
+
+    [Fact]
+    public void MixedConcepts()
+    {
+      var equation = new ASTNode(Token.Operator("-"), new List<ASTNode>
+      {
+        // Left: f(x^2 + 3) + g(sin(x) * ln(y))
+        new ASTNode(Token.Operator("+"), new List<ASTNode>
+        {
+          // f(x^2 + 3)
+          new ASTNode(Token.Function("f"), new List<ASTNode>
+          {
+            new ASTNode(Token.Operator("+"), new List<ASTNode>
+            {
+              new ASTNode(Token.Operator("^"), new List<ASTNode>
+              {
+                new ASTNode(Token.Variable("x"), new List<ASTNode>()),
+                new ASTNode(Token.Number("2"), new List<ASTNode>())
+              }),
+              new ASTNode(Token.Number("3"), new List<ASTNode>())
+            })
+          }),
+          // g(sin(x) * ln(y))
+          new ASTNode(Token.Function("g"), new List<ASTNode>
+          {
+            new ASTNode(Token.Operator("*"), new List<ASTNode>
+            {
+              new ASTNode(Token.Function("sin"), new List<ASTNode>
+              {
+                new ASTNode(Token.Variable("x"), new List<ASTNode>())
+              }),
+              new ASTNode(Token.Function("ln"), new List<ASTNode>
+              {
+                new ASTNode(Token.Variable("y"), new List<ASTNode>())
+              })
+            })
+          })
+        }),
+        // Right: 4 / (x + 1)
+        new ASTNode(Token.Operator("/"), new List<ASTNode>
+        {
+          new ASTNode(Token.Number("4"), new List<ASTNode>()),
+          new ASTNode(Token.Operator("+"), new List<ASTNode>
+          {
+            new ASTNode(Token.Variable("x"), new List<ASTNode>()),
+            new ASTNode(Token.Number("1"), new List<ASTNode>())
+          })
+        })
+      });
+      
+      Dictionary<string, double> symbolTable = new Dictionary<string, double> { { "x", 1.75 }, { "y", 30} };
+      
+      var customFunctionTable = new Dictionary<string, Func<List<double>, double>> 
+      {
+        { "f", (args) => (Math.Pow(args[0], 2) + 2 * args[0] + 3)},
+        { "g", (args) => (Math.Pow(Math.Sin(args[0]), 2) + Math.Cos(args[0]))},
+      };
+
+      var result = Calculator.Evaluate(equation, symbolTable, customFunctionTable);
+
+      Assert.Equal(49.481, result, 0.01);
+    }
+  }
+}
